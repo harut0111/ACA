35e178366acfef0b6fade4d2e3fced02b1ebf523
Harut <49102838+harut0111@users.noreply.github.com>
2019-04-11 15:33:04 +0400

    Update and rename recursion03.js to recursion03_get_integers.js

--- extra exercises/recursion03_get_integers.js
+++ extra exercises/recursion03_get_integers.js
@@ -1,3 +1,9 @@
+/* Write a JavaScript program to get the integers in range (x, y).
+
+Example : range(2, 9)
+Expected Output : [3, 4, 5, 6, 7, 8]
+ */
+
 "use strict";
 
 function findIntgers(start, end) {
