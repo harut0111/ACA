b996b43c9867d12f3ca1a0c9a755abd6c5cf66f5
Harut <49102838+harut0111@users.noreply.github.com>
2019-04-11 15:34:22 +0400

    Update recursion04_sum_array.js

--- extra exercises/recursion04_sum_array.js
+++ extra exercises/recursion04_sum_array.js
@@ -1,3 +1,8 @@
+/* Write a JavaScript program to compute the sum of an array of integers.
+
+Example : var array = [1, 2, 3, 4, 5, 6]
+Expected Output : 21 */
+
 "use strict";
 
 function sumArray(array) {
