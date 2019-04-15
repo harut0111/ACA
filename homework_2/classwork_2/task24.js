9230212ea2e141aca7d7c5b78715a4b0587afe65
Harut <49102838+harut0111@users.noreply.github.com>
2019-04-15 18:12:11 +0400

    Add files via upload

---
+++ homework_2/classwork_2/task24.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function printX(size = 8) {
+    let start = -size / 2;
+    let end = -start;
+    let txt = "";
+    // debugger;
+    for (let i = start; i <= end; i++) {
+        for (let j = start; j <= end; j++) {
+            if ( Math.abs(i) === Math.abs(j)) {
+                txt += "*";
+            } else {
+                txt += " ";
+            }
+        }
+        txt += "\n";
+    }
+    return txt;
+}
+//test
+console.log(printX(4));
+console.log(printX(6));
+console.log(printX(8));
+console.log(printX(10));
