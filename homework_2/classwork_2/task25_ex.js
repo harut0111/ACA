96f9b8be6b96b5205c5955cc475f97a7cab3db25
Harut <49102838+harut0111@users.noreply.github.com>
2019-04-16 16:15:23 +0400

    Update task25_ex.js

--- homework_2/classwork_2/task25_ex.js
+++ homework_2/classwork_2/task25_ex.js
@@ -19,3 +19,26 @@ function printX(size = 8) {
 }
 //test
 console.log(printX(18));
+
+/*
+"use strict";
+
+function printX(size = 8) {
+    let start = -size / 2;
+    let end = -start;
+    let txt = "";
+    
+    for (let i = start; i <= end; i++) {
+        for (let j = start; j <= end; j++) {
+            if (
+                 Math.abs(i) === Math.abs(j) || i === 0 || j === 0
+            ) { txt += "*"; }
+            else { txt += " "; }
+        }
+        txt += "\n";
+    }
+    return txt;
+}
+//test
+console.log(printX(18));
+*/
