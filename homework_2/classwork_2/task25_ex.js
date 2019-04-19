a8569cea172aeb5d39be243de39d5e629178d82b
Harut <49102838+harut0111@users.noreply.github.com>
2019-04-19 11:10:04 +0400

    Update task25_ex.js

--- homework_2/classwork_2/task25_ex.js
+++ homework_2/classwork_2/task25_ex.js
@@ -8,29 +8,6 @@ function printX(size = 8) {
     for (let i = start; i <= end; i++) {
         for (let j = start; j <= end; j++) {
             if (
-                 Math.abs(i) === Math.abs(j) || i === start ||
-                 i === end || j === start || j === end
-            ) { txt += "*"; }
-            else { txt += " "; }
-        }
-        txt += "\n";
-    }
-    return txt;
-}
-//test
-console.log(printX(18));
-
-/*
-"use strict";
-
-function printX(size = 8) {
-    let start = -size / 2;
-    let end = -start;
-    let txt = "";
-    
-    for (let i = start; i <= end; i++) {
-        for (let j = start; j <= end; j++) {
-            if (
                  Math.abs(i) === Math.abs(j) || i === 0 || j === 0
             ) { txt += "*"; }
             else { txt += " "; }
@@ -41,4 +18,3 @@ function printX(size = 8) {
 }
 //test
 console.log(printX(18));
-*/
