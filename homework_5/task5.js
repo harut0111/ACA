d9ed26652385070807f5dd3e46436672c2024725
Harut <49102838+harut0111@users.noreply.github.com>
2019-05-08 14:45:45 +0400

    Update task5.js

--- homework_5/task5.js
+++ homework_5/task5.js
@@ -14,3 +14,31 @@ for (const key in obj) {
 	}
 }
 console.log(obj);
+
+// Mehtod 2: using hasOwnProperty() and Object.defineProperty() methods
+/*
+for (const key in obj) {
+
+	// obj doesn't have same property name
+	if (!obj.hasOwnProperty(obj[key])) {
+		Object.defineProperty(obj, obj[key], {
+			value: key,
+			writable: true,
+			configurable: true,
+			enumerable: true
+		});
+		delete obj[key];
+
+		// obj has same property name
+	} else {
+		Object.defineProperty(obj, obj[key], {
+			value: [].concat(obj[obj[key]], key),
+			writable: true,
+			configurable: true,
+			enumerable: true
+		});
+		delete obj[key];
+	}
+}
+console.log(obj);
+*/
