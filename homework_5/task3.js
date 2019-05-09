8727036ddb816e784b0c6187be4ce28499f64bbc
Harut <49102838+harut0111@users.noreply.github.com>
2019-05-09 14:36:45 +0400

    Update task3.js

--- homework_5/task3.js
+++ homework_5/task3.js
@@ -1,11 +1,17 @@
 "use strict";
 
+//this function always returns one digit number
+
+/* WARNING! the function can leads to 
+RangeError(exceed max call stack size)
+depend on the size of given number */
+
 function getSum(num) {
     // create arr of number 
     let arrOfDig = Array.from(String(num));
     // calculate the sum of digits
     let res = arrOfDig.reduce((sum, num) => +sum + +num);
-    // if sum is not one digit call the same function, else return the sum
+    // if sum is not one digit repeat the circle, otherwise return the sum
     if (String(res).length > 1) {
         return getSum(res);
     }
