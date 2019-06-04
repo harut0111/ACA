acc4746d60068cd296f0d9e27d7a76f1030a6ce2
Harut Muqoyan <hmuqoyan@gmail.com>
2019-06-04 18:16:09 +0400

    completed

---
+++ homework_8-9/SlideShow/script.js
@@ -0,0 +1,38 @@
+
+let imgSrcArr = ['p/1.jpg', 'p/2.jpg', 'p/3.jpg', 'p/4.jpg', 'p/5.jpg', 'p/6.jpg', 'p/7.jpg']
+
+let buttons = document.getElementsByClassName('button');
+let image = document.getElementsByTagName('IMG')[0];
+
+let prev = buttons[0], play = buttons[1], pause = buttons[2], next = buttons[3]
+
+prev.addEventListener('click', subtract);
+play.addEventListener('click', slide);
+pause.addEventListener('click', slide);
+next.addEventListener('click', add);
+
+let i = 0;
+function add() {
+    i++;
+    if (i === imgSrcArr.length) { i = 0 }
+    image.src = imgSrcArr[i]
+}
+
+function subtract() {
+    i--;
+    if (i === -1) { i = imgSrcArr.length - 1 }
+    image.src = imgSrcArr[i]
+}
+
+let id, bool = true;
+function slide(ev) {
+    if (ev.target.innerHTML === 'Play') {
+        if (bool) {
+            id = setInterval(add, 1000);
+            bool = false;
+        }
+    } else {
+        clearInterval(id);
+        bool = true;
+    }
+}
