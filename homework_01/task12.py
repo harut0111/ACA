771666fc48f351fbb5fd72a41d74731a6f7b0415
Harutyun Muqoyan <hmuqoyan@gmail.com>
2020-01-30 18:35:44 +0400

    completed

--- homework_01/task12.py
+++ homework_01/task12.py
@@ -4,4 +4,4 @@ def get_area(name, n1,n2):
     elif name == "rectangle": return n1 * n2
     return "check the name"
 print(get_area('triangle', 6, 7))
-print(get_area('rectangle', 6, 7))
+print(get_area('rectangle', 8, 5))
