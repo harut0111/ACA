f4980d314b902a6a2ef676409c9832a8cffb0fa7
Harutyun Muqoyan <hmuqoyan@gmail.com>
2020-02-03 13:58:15 +0400

    added a new task

--- mysql_test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-    user_id VARCHAR(10),
-    first_name VARCHAR(10),
-    last_name VARCHAR(10),
-    email VARCHAR(20),
-    age INT(5),
-    password VARCHAR(10)
-)
