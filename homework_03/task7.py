4d40d260ef6f8b1a5416108b66b8c6a6a3889273
Harut Muqoyan <hmuqoyan@gmail.com>
2020-03-05 18:47:39 +0400

    completed

--- homework_03/task7.py
+++ homework_03/task7.py
@@ -7,11 +7,15 @@ def prodInArr(arrOfarr):
         return False
 
     for arr in arrOfarr:
+        if not isinstance(arr, list): return "Invalid Argument"
         curr = getMin(arr)
         if curr: res *= curr
+    if res == 1: return "No Negatives"
     return res
 
 array1 = [[2, -9, -3, 0], [1, 2], [-4, -11, 0]]
 array2 = [[3, 4], [11, 0], [5, 6, 7, 8]]
+array3 = [1, 2, 3]
 print(prodInArr(array1))
 print(prodInArr(array2))
+print(prodInArr(array3))
