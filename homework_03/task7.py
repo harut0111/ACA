2ee3585dccf87d207c2e233b131bf1a88511fcfb
Harut Muqoyan <hmuqoyan@gmail.com>
2020-03-05 18:23:45 +0400

    completed

---
+++ homework_03/task7.py
@@ -0,0 +1,17 @@
+def prodInArr(arrOfarr):
+    res = 1
+
+    def getMin(arr):
+        minNum = min(arr)
+        if minNum < 0: return minNum
+        return False
+
+    for arr in arrOfarr:
+        curr = getMin(arr)
+        if curr: res *= curr
+    return res
+
+array1 = [[2, -9, -3, 0], [1, 2], [-4, -11, 0]]
+array2 = [[3, 4], [11, 0], [5, 6, 7, 8]]
+print(prodInArr(array1))
+print(prodInArr(array2))
