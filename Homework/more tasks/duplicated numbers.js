d3de9ba395fe76d2fde2ae5bab3ef1e1cf55cb24
Harut Muqoyan <hmuqoyan@gmail.com>
2021-02-02 22:08:01 +0400

    added new tasks

--- Homework/more tasks/duplicated numbers.js
+++ Homework/more tasks/duplicated numbers.js
@@ -2,33 +2,38 @@
 then will be difference numbers for 
 indexOf() and lastIndexOf() function. */
 
-function filterDuplicated(arr) {
-  const uniqueNum = Array.from(new Set(arr));
-  const result = [];
-  for (const num of uniqueNum) {
-    if (arr.indexOf(num) !== arr.lastIndexOf(num)) {
-      result.push(num);
-    }
-  }
-  return result;
-}
+const retrieveDuplicated = (arr) => (
+  arr.filter((item, index) => arr.indexOf(item) === index)
+)
 
-function filterNonDuplicated(arr) {
-  const uniqueNum = Array.from(new Set(arr));
-  const result = [];
-  for (const num of uniqueNum) {
-    if (arr.indexOf(num) === arr.lastIndexOf(num)) {
-      result.push(num);
-    }
-  }
-  return result;
-}
+const retrieveNonDuplicated = (arr) => (
+  arr.filter((item, index) => arr.indexOf(item) !== index)
+)
 
-const arr = [1, 2, 2, 4, 5, 8, 9, 2, 23, 7, 5, 6, 6];
-console.log("filterNonDuplicated(arr)", filterNonDuplicated(arr));
+const arr = [1, 2, 2, 4, 5, 8, 9, 2, 23, 7, 5, 6, 6, 1];
+console.log("retrieveNonDuplicated(arr)", retrieveNonDuplicated(arr));
 
+// function retrieveDuplicated(arr) {
+//   const uniqueNum = Array.from(new Set(arr));
+//   const result = [];
+//   for (const num of uniqueNum) {
+//     if (arr.indexOf(num) !== arr.lastIndexOf(num)) {
+//       result.push(num);
+//     }
+//   }
+//   return result;
+// }
 
-
+// function filterNonDuplicated(arr) {
+//   const uniqueNum = Array.from(new Set(arr));
+//   const result = [];
+//   for (const num of uniqueNum) {
+//     if (arr.indexOf(num) === arr.lastIndexOf(num)) {
+//       result.push(num);
+//     }
+//   }
+//   return result;
+// }
 
 // ------------>   another solution using regExp   <-------------
 function nonDuplicated(array) {
