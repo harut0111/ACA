e5a70ab7b4ffff4228be2ec42d85bb04a6924c49
Harut Muqoyan <hmuqoyan@gmail.com>
2021-02-03 00:19:24 +0400

    edited

--- Homework/more tasks/06.Bubble Sort.js
+++ Homework/more tasks/06.Bubble Sort.js
@@ -28,10 +28,10 @@ console.log(bubbleSort(arr));
 
 /*  To calculate the complexity of the bubble sort algorithm, it 
 is useful to determine how many comparisons each loop performs.
-For each element in the array, bubble sort does n-1n−1 comparisons.
-In big O notation, bubble sort performs O(n)O(n) comparisons. 
-Because the array contains nn elements, it has an O(n)O(n) number of elements.
-In other words, bubble sort performs O(n)O(n) operations on an O(n)O(n) 
+For each element in the array, bubble sort does n-1 comparisons.
+In big O notation, bubble sort performs O(n) comparisons. 
+Because the array contains nn elements, it has an O(n) number of elements.
+In other words, bubble sort performs O(n) operations on an O(n)
 number of elements, leading to a total running time of O(n^2)   */ 
 
 // https://brilliant.org/wiki/bubble-sort/#:~:text=1%20return%20array-,Complexity%20of%20Bubble%20Sort,)%20O(n)%20comparisons.
