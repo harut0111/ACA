e9f533e273ed047c64095bd0ab600bdca99e46cb
Harut Muqoyan <hmuqoyan@gmail.com>
2021-02-04 19:26:00 +0400

    edited

--- Homework/more tasks/06.Bubble Sort.js
+++ Homework/more tasks/06.Bubble Sort.js
@@ -1,30 +1,27 @@
-"use strict";
 
-function bubbleSort(array) {
-    let end = array.length - 1;
-    let bool = true;
-    while(bool) {
-        bool = false;
-        debugger;
-        for (let i = 0; i < end; i++) {
-            if (array[i] > array[i + 1]) {
-                [array[i], array[i + 1]] = [array[i + 1], array[i]];
-                bool= true;
-            }
-        }
-        if (bool) {end--}
+function bubbleSort(arr) {
+  let end = arr.length - 1;
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < end; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
     }
-    return array;
+    swapped && end--; // for optimization 
+  }
+  return arr;
 }
 //test
 let arr = [];
 for (let i = 0; i < 10; i++) {
-    let randomNum = Math.floor((Math.random() * 51) - 25);
-    arr.push(randomNum)
+  let randomNum = Math.floor(Math.random() * 51 - 25);
+  arr.push(randomNum);
 }
 console.log(arr);
 console.log(bubbleSort(arr));
-
 
 /*  To calculate the complexity of the bubble sort algorithm, it 
 is useful to determine how many comparisons each loop performs.
@@ -32,6 +29,6 @@ For each element in the array, bubble sort does n-1 comparisons.
 In big O notation, bubble sort performs O(n) comparisons. 
 Because the array contains nn elements, it has an O(n) number of elements.
 In other words, bubble sort performs O(n) operations on an O(n)
-number of elements, leading to a total running time of O(n^2)   */ 
+number of elements, leading to a total running time of O(n^2)   */
 
 // https://brilliant.org/wiki/bubble-sort/#:~:text=1%20return%20array-,Complexity%20of%20Bubble%20Sort,)%20O(n)%20comparisons.
