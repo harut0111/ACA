e8c811ebb6c6c69eba6b4fa4aeaa6de8fb6f6a86
Harut Muqoyan <hmuqoyan@gmail.com>
2024-11-06 12:06:21 +0400

    docs: added new file

---
+++ Homework/homework_03/solutions.js
@@ -0,0 +1,26 @@
+/* ------------------------ Homework 2 (review) --------------------*/
+/*  4 Nov 2024   */
+
+/* ------------ Task 1 ------------ */
+const findSecondMax = () => {};
+
+// /* Test */
+// const inputArray = [
+//   [[1, 2, 3, 4], 1, 3],
+//   [[1, 2, 3, 4], 2, 1],
+//   [[1], 1, 3],
+// ];
+// const outputArray = [
+//   [1, 1, 1, 1, 2, 3, 4, 4, 4, 4],
+//   [1, 2, 1, 2, 3, 4, 3, 4],
+//   [1, 1, 1, 1, 1, 1, 1],
+// ];
+
+// for (let i = 0; i < inputArray.length; i++) {
+//   const result = findSecondMax(...inputArray[i]);
+//   const test =
+//     JSON.stringify(result) === JSON.stringify(outputArray[i])
+//       ? 'Passed'
+//       : 'Failed';
+//   console.log(test);
+// }
