ef6b53168f76ca5d47b01aa965c36222f290ba53
Harut Muqoyan <hmuqoyan@gmail.com>
2024-11-13 16:28:28 +0400

    docs: add new nots

---
+++ Homeworks/more tasks/notes.js
@@ -0,0 +1,12 @@
+/* -------> Calculate number of digits in a number
+
+    Math.floor(Math.log10(num)) + 1
+    e.g., 234 has 3 digits: Math.log10(234) ≈ 2.37, floor + 1 = 3
+    Math.log10(100) is equal to 10^2 and +1 will be 3
+*/
+
+/* -------> Number to Array of digits
+
+   const arrOfDigits = Array.from(currentNum.toString(), (digit) => +digit);
+   e.g., 12345 -> ['1', '2', '3', '4', '5'] -> [1, 2, 3, 4, 5]
+*/
