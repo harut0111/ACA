d9baac127f7a1dd416f8ddca1428d1e355174f88
Harut Muqoyan <hmuqoyan@gmail.com>
2024-11-23 00:31:52 +0400

    docs: update

--- Homeworks/more problems/06.printTriangle.js
+++ Homeworks/more problems/06.printTriangle.js
@@ -1,22 +1,16 @@
-"use strict";
-
-function printTriangle(n = 9) {
-    let d = n - 1;
-    let row = "";
-    for (let i = d; i >= 0 ; i--) {
-        for (let j = -d; j <= d; j++) {
-            if (Math.abs(i) + Math.abs(j) <= d) {
-                row += "*";
-            } else {
-                row += " ";  
-            }
-        }
-        row += "\n";
+const printTriangle = (n = 9) => {
+  let d = n - 1;
+  let row = '';
+  for (let i = d; i >= 0; i--) {
+    for (let j = -d; j <= d; j++) {
+      if (Math.abs(i) + Math.abs(j) <= d) {
+        row += '*';
+      } else {
+        row += ' ';
+      }
     }
-    return row;
-}
-//test
-let array = [3, 5, 9]
-for (let i = 0; i < array.length; i++) {
-    console.log(printTriangle(array[i]));   
-}
+    row += '\n';
+  }
+  return row;
+};
+console.log(printTriangle(9));
