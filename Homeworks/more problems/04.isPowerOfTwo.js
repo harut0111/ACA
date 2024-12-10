9e4af5b9433eadc02d0d2c2d9c498972108ad2f3
Harut Muqoyan <hmuqoyan@gmail.com>
2024-12-10 23:36:30 +0400

    docs: update

--- Homeworks/more problems/04.isPowerOfTwo.js
+++ Homeworks/more problems/04.isPowerOfTwo.js
@@ -1,14 +1,15 @@
 // To check it let's Use Bitwise operators
 
-// const decimalToBinary = (decimal) => decimal.toString(2)
-// console.log('decimalToBinary(4)', decimalToBinary(4))
-
 // N    = 4,  000100
 // N-1  = 3,  000011
 // N & N-1 -> 000000 // !0 is true
 
 // If a number is a power of 2 then then N & (N-1) will be zero.
 // The only exception to above rule is when N is Zero
+
+// // Convert to decimal representation
+// const decimalToBinary = (decimal) => decimal.toString(2)
+// console.log('decimalToBinary(4)', decimalToBinary(4))
 
 const isPowerOfTwo = (n) => !(n & (n - 1));
 
